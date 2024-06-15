f6781ff9a9255b20c25915131d6d1ad4cde48b44
Andrew Lamoureux <lwerdna@users.noreply.github.com>
2024-06-15 10:22:26 -0400

    Migrate Yusupov Ch1 to lichess format

---
+++ lichess-dl.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget https://lichess.org/study/vmzEymCY.pgn -O './pgns/yusupov-build-chess-fundamentals.pgn'
+wget https://lichess.org/study/KIGcwIGN.pgn -O './pgns/all-openings.pgn'
--- pgns/lichess-dl.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-wget https://lichess.org/study/vmzEymCY.pgn -O 'yusupov-build-chess-fundamentals.pgn'
-wget https://lichess.org/study/KIGcwIGN.pgn -O 'all-openings.pgn'
